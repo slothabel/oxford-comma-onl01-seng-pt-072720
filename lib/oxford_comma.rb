958509afb65e9243c4470aa469aacfc1e620c0ce
Yoshi Mejia <yoshimejia00@gmail.com>
2020-08-05 20:18:05 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
 
+if array.length < 2
+return array.join
+
+  elsif array.length == 2
+    new_array = [array.first, "and", array.last]
+    new_array.join(" ")
+ else array.length == 3
+   #newest_array = array[0], array[1], array[-1]
+   #newest_array.join(", ")
+   array[-1] = "and " + array[-1]
+   array.join(", ")
+  end
+
 end
